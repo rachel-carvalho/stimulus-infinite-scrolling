9cefe4226db31a78fc55c832665d1e8a17869fb6
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-01-22 11:15:14 -0500

    reword test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'gets index' do
     get root_url
     assert_response :success
   end
-
 end
