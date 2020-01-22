49d9e31effdb749e15044c61de91428699fc87db
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-01-22 11:47:08 -0500

    render items only when request comes from xhr

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,5 +1,6 @@
 class ThingsController < ApplicationController
   def index
     @items = ['an item'] * 5
+    render partial: 'item', collection: @items if request.xhr?
   end
 end
--- test/controllers/things_controller_test.rb
+++ test/controllers/things_controller_test.rb
@@ -7,4 +7,11 @@ class ThingsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'h1', 'Long and inifinte listing'
     assert_select 'ol li', 5
   end
+
+  test 'gets items only in ajax' do
+    get root_url, xhr: true
+    assert_select 'h1', 0
+    assert_select 'ol', 0
+    assert_select 'li', 5
+  end
 end
