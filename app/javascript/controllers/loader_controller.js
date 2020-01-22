f9116688a8e94648dd86596d5e750f9700ac0e9e
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-01-22 11:11:04 -0500

    store current page

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -3,6 +3,14 @@ import { Controller } from 'stimulus'
 export default class Loader extends Controller {
   static targets = ['list', 'button', 'loading']
 
+  get page() {
+    return parseInt(this.data.get('page') || (this.page = 1))
+  }
+
+  set page(value) {
+    return this.data.set('page', value)
+  }
+
   connect() {
   }
 
