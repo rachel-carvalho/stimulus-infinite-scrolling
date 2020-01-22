f10a0831be8fca4357f6eb861b77861cb68ea883
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-01-22 11:02:57 -0500

    add fake loading action

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -1,9 +1,13 @@
 import { Controller } from 'stimulus'
 
 export default class Loader extends Controller {
-  static targets = ['list', 'button']
+  static targets = ['list', 'button', 'loading']
 
   connect() {
+  }
 
+  load() {
+    this.loadingTarget.style.display = 'block'
+    this.buttonTarget.disabled = true
   }
 }
