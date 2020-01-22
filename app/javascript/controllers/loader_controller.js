b79ada2315e58c29854f73c16c2b90232feb0b89
Rachel Carvalho <rachel.carvalho@gmail.com>
2020-01-22 10:54:47 -0500

    add basic markup and css to home, remove example stuff from loader controller

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -1,18 +1,9 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
+import { Controller } from 'stimulus'
 
-import { Controller } from "stimulus"
-
-export default class extends Controller {
-  static targets = [ "output" ]
+export default class Loader extends Controller {
+  static targets = ['list', 'button']
 
   connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
+
   }
 }
